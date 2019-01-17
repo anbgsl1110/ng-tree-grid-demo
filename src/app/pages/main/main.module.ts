7360abcb07fa5c663ad7d4fb459a4ab63cd039f9
anbgsl1110 <anbgsl1110@gmail.com>
2019-01-17 14:12:47 +0800

    tree控件

--- src/app/pages/main/main.module.ts
+++ src/app/pages/main/main.module.ts
@@ -5,9 +5,10 @@ import {NgZorroAntdModule} from "ng-zorro-antd";
 import { LeftControlComponent } from './left-control/left-control.component';
 import {MainRoutingModule} from "./main-routing.module";
 import { RightControlComponent } from './right-control/right-control.component';
+import { TreeComponent } from './left-control/tree/tree.component';
 
 @NgModule({
-  declarations: [MainComponent, LeftControlComponent, RightControlComponent],
+  declarations: [MainComponent, LeftControlComponent, RightControlComponent, TreeComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
